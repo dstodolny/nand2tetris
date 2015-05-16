b8d49309dac68b20d691902bd7fa5cc6208259b2
Dominik Stodolny <d.stodolny@gmail.com>
2015-05-16 20:26:18 +0200

    implement low level multiplication

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -7,3 +7,41 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+@i
+M=0 // i = 0
+
+@result
+M=0 // result = 0
+
+(LOOP)
+  @i
+  D=M
+  @R1
+  D=D-M
+  @STOP
+  D;JEQ // if i = b goto STOP
+
+  @result
+  D=M
+  @R0
+  D=D+M
+  @result
+  M=D // result = result + b
+
+  @i
+  M=M+1 // i = i + 1
+  @LOOP
+  0;JMP
+
+(STOP)
+  @result
+  D=M
+  @R2
+  M=D // R2 = result
+
+(END)
+  @END
+  0;JMP
+
+
+
