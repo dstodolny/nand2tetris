b5b3e30ddfcdf439eb1141cc3e19f61b2cd79f73
Dominik Stodolny <d.stodolny@gmail.com>
2015-05-16 21:56:05 +0200

    implement fill the screen program

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -9,3 +9,51 @@
 // program clears the screen, i.e. writes "white" in every pixel.
 
 // Put your code here.
+
+(LOOP)
+  @SCREEN
+  D=A
+  @address
+  M=D // set address to the base address of Hack's screen
+
+  @KBD
+  D=M
+  
+  @KEYUP
+  D;JEQ // if no key is pressed goto KEYUP
+
+  @KEYDOWN // if any key is pressed goto KEYDOWN
+  0;JMP
+
+(DRAW)
+  @address
+  D=M
+  @KBD // end of the screen 
+  D=D-A
+  @LOOP
+  D;JEQ
+
+  @color
+  D=M
+  @address
+  A=M
+  M=D // color the screen 
+  
+  @address
+  M=M+1
+  
+  @DRAW
+  0;JMP
+
+(KEYUP)
+  @color
+  M=0 // set color to white
+  @DRAW
+  0;JMP
+
+(KEYDOWN)
+  @color
+  M=-1 // set color to black
+  @DRAW
+  0;JMP
+
