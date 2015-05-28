41aac2538994dd23539a0bacbb8a939db3576383
Dominik Stodolny <d.stodolny@gmail.com>
2015-05-28 03:48:27 +0200

    add symbols feature

--- 06/HackAssembler.rb
+++ 06/HackAssembler.rb
@@ -21,11 +21,13 @@ JUMP = { nil => "000", "JGT" => "001", "JEQ" => "010",
 
 # Encapsulates access to the input code
 class Parser
+  attr_accessor :line
   attr_reader :file, :current_command
 
   def initialize(filename)
     @file = File.open(filename, "r")
     @current_command = ""
+    @line = 0
   end
 
   def no_more_commands?
@@ -37,6 +39,7 @@ class Parser
       @current_command = file.readline.split("//").first.strip
       break unless @current_command.empty? && !no_more_commands?
     end
+    @line += 1
   end
 
   def command_type
@@ -70,23 +73,59 @@ class Parser
     return nil unless @current_command.include?(";")
     @current_command.split(";").last
   end
+
+  def rewind
+    @file.rewind
+    @line = 0
+  end
 end
+
+class String
+  def numeric?
+    Float(self) != nil rescue false
+  end
+end
+
+symbols = { "SP" => "0", "LCL" => "1", "ARG" => "2", "THIS" => "3",
+            "THAT" => "4", "R0" => "0", "R1" => "1", "R2" => "2",
+            "R3" => "3", "R4" => "4", "R5" => "5", "R6" => "6",
+            "R7" => "7", "R8" => "8", "R9" => "9", "R10" => "10",
+            "R11" => "11", "R12" => "12", "R13" => "13",
+            "R14" => "14", "R15" => "15", "SCREEN" => "16384",
+            "KBD" => "24576" }
+var_count = 16
 
 filename = ARGV.first
 p = Parser.new(filename)
 
 target = File.open(filename.split(".").first + ".hack", "w")
 
+until p.no_more_commands?
+  p.advance
+  if p.command_type == "L_COMMAND"
+    symbols[p.symbol] = p.line - 1
+    p.line -= 1
+  end
+end
+
+p.rewind
 
 until p.no_more_commands?
   p.advance
+  next if p.command_type == "L_COMMAND"
   if p.command_type == "C_COMMAND"
     target.write("111")
     target.write COMP[p.comp]
     target.write DEST[p.dest]
     target.write JUMP[p.jump]
-  else
+  elsif p.symbol.numeric?
     target.write("%016b" % p.symbol.to_i)
+  else
+    unless symbols.include?(p.symbol)
+      symbols[p.symbol] = var_count
+      var_count += 1
+    end
+    target.write("%016b" % symbols[p.symbol].to_i)
   end
   target.write("\n")
 end
